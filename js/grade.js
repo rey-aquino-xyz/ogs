bc9d4c97d5f56fe9e009c51c704e80c647ddaeff
Rey Aquino <rey.aquino.dev@gmail.com>
2022-05-20 23:41:10 +0800

    --Update

--- js/grade.js
+++ js/grade.js
@@ -1,21 +1,6 @@
 $(document).ready(function () {
 
-    $("[name='lrn-form']").on('submit', function (e) {
-        e.preventDefault();
-        var lrn = $("#lrn");
-        var lrnpwd = document.getElementById("lrnlock");
-        $.ajax({
-            url: '../../includes/grade.php',
-            data: {lrn: lrn.val()},
-            processData: false,
-            contentType: false,
-            type: 'POST',
-            sucess: function (r) {
-                if (r == "t") {
-                    lrnpwd.style.display = "block";
-                }
-            }
-        });
+    $("#grade-content").load('lrn.php');
 
-    });
+
 });
