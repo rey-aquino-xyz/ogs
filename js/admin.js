e44282d384b7635a523396e8e3e5dcad4108778e
Rey Aquino <rey.aquino.dev@gmail.com>
2022-05-21 15:42:18 +0800

    Update

--- js/admin.js
+++ js/admin.js
@@ -2,13 +2,17 @@ $(document).ready(function () {
 
     $("#admin-content").load("../../views/admin/files.php");
 
-    $("#fileslink").on('click', function(e){
+    $("#fileslink").on('click', function (e) {
         e.preventDefault();
         $("#admin-content").load("../../views/admin/files.php");
     })
 
-   
+    $("[name ='myInfoLink']").on('click', function (e) {
+        e.preventDefault();
+        $("#admin-content").load("../../views/admin/account.php");
 
-   
+    })
+
+
 
 });
